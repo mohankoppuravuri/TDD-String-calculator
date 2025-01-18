b6758bfe27a9662ee14e9c764cf589235854be4e
mohan-triomics <mohan.krishna@triomics.in>
2025-01-18 13:19:13 +0530

    Add Test 2: (1,2 = 3)

--- main_test.ts
+++ main_test.ts
@@ -4,3 +4,7 @@ import { add } from "./main.ts";
 Deno.test("Test 1", function addTest() {
   assertEquals(add(""), 0);
 });
+
+Deno.test("Test 2", function addTest() {
+  assertEquals(add("1,2"), 3);
+});
