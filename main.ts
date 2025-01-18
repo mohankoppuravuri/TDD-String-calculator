01fd91515caa52013583878e8ebc2833abd4fb1a
mohan-triomics <mohan.krishna@triomics.in>
2025-01-18 14:08:58 +0530

    :bug: Fix test 8

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-export function add(numbers: string): number {
-  const numbersArray = numbers.split(/[\n,]/);
+function sumOfArray(numbersArray: string[]): number {
   return numbersArray.reduce((acc, currentNumber) => {
     return acc + Number(currentNumber ?? 0);
   }, 0);
+}
+
+export function add(numbers: string): number {
+  const firstLine = numbers.split(/[\n]/)[0];
+  if (firstLine.substring(0, 2) === "//") {
+    // Custom delimiter
+    const delimiter = firstLine.substring(2);
+    const numbersArray = numbers.split(/[\n]/)[1].split(delimiter);
+    return sumOfArray(numbersArray);
+  }
+  const numbersArray = numbers.split(/[\n,]/);
+  return sumOfArray(numbersArray);
 }
