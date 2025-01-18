a42f3a9ad24454940ad3cce41907788cfbdbb6af
mohan-triomics <mohan.krishna@triomics.in>
2025-01-18 13:49:32 +0530

    :bug: Fix test 7.

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
-  const numbersArray = numbers.split(",");
+  const numbersArray = numbers.split(/[\n,]/);
   return numbersArray.reduce((acc, currentNumber) => {
     return acc + Number(currentNumber ?? 0);
   }, 0);
