69f274619732304791852e933a4851ce3f2a29e9
mohan-triomics <mohan.krishna@triomics.in>
2025-01-18 13:34:30 +0530

    :zap: Add test 3 and test 4

--- main_test.ts
+++ main_test.ts
@@ -8,3 +8,11 @@ Deno.test("Test 1", function addTest() {
 Deno.test("Test 2", function addTest() {
   assertEquals(add("1,2"), 3);
 });
+
+Deno.test("Test 3", function addTest() {
+  assertEquals(add("0,2"), 2);
+});
+
+Deno.test("Test 4", function addTest() {
+  assertEquals(add("2"), 2);
+});
